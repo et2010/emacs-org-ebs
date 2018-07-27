942788c8cfc48ee3b8b85fd87ce4fda0dabb39cf
et2010 <jianwang.academic@gmail.com>
2018-07-27 15:06:05 +0800

    Update file header

--- org-ebs.el
+++ org-ebs.el
@@ -1,20 +1,28 @@
-;; Evidence-Based Scheduling for Emacs Org Mode
+;;; org-ebs.el --- Bypass that mental block when writing your papers. -*- lexical-binding: t -*-
+
 ;; Copyright (C) 2017 Scott Weldon
-;;
-;; This program is free software: you can redistribute it and/or modify
+
+;; Author: Scott Weldon
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24"))
+;; Keywords: convenience
+;; Homepage: https://github.com/et2010/org-ebs
+
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;;
+
+;;; Commentary:
+
 ;; Evidence-Based Scheduling:
 ;; 1. Estimate time for tasks (as granular as possible).
 ;;    Here using Org-Mode `Effort` property.
